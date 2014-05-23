c5c998957812df3ea801a76bac49886b869453c7
Tiago Bastos <contact@tiagobastos.me>
2014-05-23 12:08:46 -0300

    Refator to support iOS7.1
    * Change deployment target;
    * Remove deprecation warnings;
    * Remove iOS6 gradients.

--- CMNavBarNotificationView/CMNavBarNotificationView.h
+++ CMNavBarNotificationView/CMNavBarNotificationView.h
@@ -2,6 +2,7 @@
 //  CMNavBarNotificationView.h
 //  Moped
 //
+//  Modified by Tiago Bastos on 23/05/14.
 //  Modified by Eduardo Pinho on 1/12/13.
 //  Created by Engin Kurutepe on 1/4/13.
 //  Copyright (c) 2013 Codeminer42 All rights reserved.
@@ -13,51 +14,56 @@
 extern NSString *kCMNavBarNotificationViewTapReceivedNotification;
 
 typedef void (^CMNotificationSimpleAction)(id);
+
 @protocol CMNavBarNotificationViewDelegate;
+
 @class OBGradientView;
+
 @interface CMNavBarNotificationView : UIView
 
-@property (nonatomic, strong) UILabel *textLabel;
-@property (nonatomic, strong) UILabel *detailTextLabel;
-@property (nonatomic, strong) UIImageView *imageView;
-@property (nonatomic, assign) id<CMNavBarNotificationViewDelegate> delegate;
-@property (nonatomic, strong) UIView * contentView;
+@property(nonatomic, strong) UILabel *textLabel;
+@property(nonatomic, strong) UIColor *textColor;
+@property(nonatomic, strong) UILabel *detailTextLabel;
+@property(nonatomic, strong) UIImageView *imageView;
+@property(nonatomic, assign) id<CMNavBarNotificationViewDelegate> delegate;
+@property(nonatomic, strong) UIView *contentView;
 
-@property (nonatomic) NSTimeInterval duration;
+@property(nonatomic) NSTimeInterval duration;
 
-+ (CMNavBarNotificationView *) notifyWithText:(NSString*)text
-                                 detail:(NSString*)detail
-                                  image:(UIImage*)image
-                            andDuration:(NSTimeInterval)duration;
++ (CMNavBarNotificationView *)notifyWithText:(NSString *)text
+                                      detail:(NSString *)detail
+                                       image:(UIImage *)image
+                                 andDuration:(NSTimeInterval)duration;
 
-+ (CMNavBarNotificationView *) notifyWithText:(NSString*)text
-                                 detail:(NSString*)detail
-                            andDuration:(NSTimeInterval)duration;
++ (CMNavBarNotificationView *)notifyWithText:(NSString *)text
+                                      detail:(NSString *)detail
+                                 andDuration:(NSTimeInterval)duration;
 
-+ (CMNavBarNotificationView *) notifyWithText:(NSString*)text
-                              andDetail:(NSString*)detail;
++ (CMNavBarNotificationView *)notifyWithText:(NSString *)text
+                                   andDetail:(NSString *)detail;
 
-+ (CMNavBarNotificationView *) notifyWithText:(NSString*)text
-                                 detail:(NSString*)detail
-                                  image:(UIImage*)image
-                               duration:(NSTimeInterval)duration
-                          andTouchBlock:(CMNotificationSimpleAction)block;
++ (CMNavBarNotificationView *)notifyWithText:(NSString *)text
+                                      detail:(NSString *)detail
+                                       image:(UIImage *)image
+                                    duration:(NSTimeInterval)duration
+                               andTouchBlock:(CMNotificationSimpleAction)block;
 
-+ (CMNavBarNotificationView *) notifyWithText:(NSString*)text
-                                 detail:(NSString*)detail
-                               duration:(NSTimeInterval)duration
-                          andTouchBlock:(CMNotificationSimpleAction)block;
++ (CMNavBarNotificationView *)notifyWithText:(NSString *)text
+                                      detail:(NSString *)detail
+                                    duration:(NSTimeInterval)duration
+                               andTouchBlock:(CMNotificationSimpleAction)block;
 
-+ (CMNavBarNotificationView *) notifyWithText:(NSString*)text
-                                 detail:(NSString*)detail
-                          andTouchBlock:(CMNotificationSimpleAction)block;
++ (CMNavBarNotificationView *)notifyWithText:(NSString *)text
+                                      detail:(NSString *)detail
+                               andTouchBlock:(CMNotificationSimpleAction)block;
 
-+ (void) setBackgroundImage:(UIImage *)image;
-- (void) setBackgroundColor:(UIColor *)color;
++ (void)setBackgroundImage:(UIImage *)image;
+- (void)setBackgroundColor:(UIColor *)color;
+- (void)setTextColor:(UIColor *)color;
 
 @end
 
-@protocol CMNavBarNotificationViewDelegate <NSObject>
+@protocol CMNavBarNotificationViewDelegate<NSObject>
 
 @optional
 - (void)didTapOnNotificationView:(CMNavBarNotificationView *)notificationView;
